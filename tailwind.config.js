45e5b998e3a92670150cabb51639f170c0e98e1e
Joao Teles <joaojoseteles@gmail.com>
2023-02-28 18:34:07 -0300

    :recycle: refactor: arquivos utilizando font-family do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
   content: [
     "src/pages/**/*.{js,ts,jsx,tsx}",
     "src/components/**/*.{js,ts,jsx,tsx}",
-    "src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -13,45 +12,50 @@ module.exports = {
           DEFAULT: "1rem",
           sm: "2rem",
         },
-        screens: {
-          sm: "600px",
-          md: "728px",
-          lg: "998px",
-          xl: "1140px",
-        },
       },
-      fontFamily: {
-        sans: "Space Grotesk, sans-serif",
+
+      backgroundImage: {
+        mobile: "url(/Footer/mobile.png)",
+        desktop: "url(/Footer/desktop.png)",
       },
-      colors: {
-        black: {
-          500: "#000000",
-          400: "#1D1D1D",
-        },
-        gray: {
-          500: "#313131",
-          400: "#7676761A",
-          300: "#777777",
-        },
-        white: {
-          500: "#FFFFFF",
-          400: "#FAFAFA",
-          300: "#D9D9D9",
-        },
-        green: {
-          500: "#1E5720",
-          400: "#E3F1E4",
-        },
-        red: {
-          500: "#8F2929",
-          400: "#A46767",
-        },
-        yellow: {
-          500: "#F5C731",
-        },
-        blue: {
-          500: "#11569E",
-        },
+    },
+    fontFamily: {
+      spaceGrotesk: ["Space Grotesk", "sans-serif"],
+    },
+    screens: {
+      sm: "600px",
+      md: "728px",
+      lg: "998px",
+      xl: "1140px",
+    },
+    colors: {
+      black: {
+        500: "#000000",
+        400: "#1D1D1D",
+      },
+      gray: {
+        500: "#313131",
+        400: "#7676761A",
+        300: "#777777",
+      },
+      white: {
+        500: "#FFFFFF",
+        400: "#FAFAFA",
+        300: "#D9D9D9",
+      },
+      green: {
+        500: "#1E5720",
+        400: "#E3F1E4",
+      },
+      red: {
+        500: "#8F2929",
+        400: "#A46767",
+      },
+      yellow: {
+        500: "#F5C731",
+      },
+      blue: {
+        500: "#11569E",
       },
     },
   },
