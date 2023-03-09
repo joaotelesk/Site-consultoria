49b74ef68bdda4003079a7c65150713788258895
Joao Teles <joaojoseteles@gmail.com>
2023-03-08 22:56:08 -0300

    :recycle: refactor: utilizando ApolloProvider e ChakraProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout/Layout";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "@/lib/apollo";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
