73f65e5d620079effd126ff38255e6308002d71d
Joao Teles <joaojoseteles@gmail.com>
2023-03-09 22:20:47 -0300

    :sparkles: feat: Pasta de arquivos de interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,20 @@
+export interface Thumbnail {
+  id: string;
+  url: string;
+}
+
+export interface Post {
+  author: string;
+  title: string;
+  description: string;
+  content: {
+    markdown: string;
+    html: string;
+  };
+  postType: "dicas" | "italia" | "portugal";
+  createdAt: Date;
+  updatedAt: Date;
+  slug: string;
+  thumbnail: Thumbnail;
+  thumbnailDescription: string;
+}
