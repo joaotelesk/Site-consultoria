8dd1b21fb9214a0e5d6297f03acfa3d109cbfe32
Joao Teles <joaojoseteles@gmail.com>
2023-03-09 22:20:03 -0300

    :sparkles: feat: Pasta de arquivos Utilities

---
+++ src/Utilities/ServiceData.ts
@@ -0,0 +1,26 @@
+export const ServiceData = [
+  {
+    title: "Cidadania Portuguesa",
+    description:
+      "Oferecemos uma variedade de serviços para os nossos assessorados que buscam o sonho de viver em Portugal e Itália!",
+    link: "#",
+    image: "/Home/service1.png",
+    alt: "imagem de uma mulher referente a servicos cidadania Portuguesa",
+  },
+  {
+    title: "Cidadania Italina",
+    description:
+      "Oferecemos uma variedade de serviços para os nossos assessorados que buscam o sonho de viver em Portugal e Itália!",
+    link: "#",
+    image: "/Home/service2.png",
+    alt: "imagem de uma mulher referente a servicos cidadania Portuguesa",
+  },
+  {
+    title: "Visto Portugues",
+    description:
+      "Oferecemos uma variedade de serviços para os nossos assessorados que buscam o sonho de viver em Portugal e Itália!",
+    link: "#",
+    image: "/Home/service3.svg",
+    alt: "imagem de uma mulher referente a servicos cidadania Portuguesa",
+  },
+];
