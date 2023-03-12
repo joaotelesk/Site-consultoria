c2afb960e7e737e2f279815b14028b418ed4d103
Joao Teles <joaojoseteles@gmail.com>
2023-03-12 20:16:58 -0300

    :recycle: refactor: useService para useServiceMutation e feat: useServiceQuery

--- src/Utilities/Services.ts
+++ src/Utilities/Services.ts
@@ -1,15 +1,19 @@
 import { useToast } from "@chakra-ui/react";
 import { useMutation } from "@apollo/client";
+import { client } from "@/lib/apollo";
 import {
   FORM_MUTATION,
   SUBSCRIBE_MUTATION,
 } from "@/graphql/mutations/mutation";
+import { GET_FIRST_FOUR_POSTS_QUERY } from "@/graphql/queries/query";
+import { Post } from "@/interfaces";
 
 interface ServiceResponse {
   success: boolean;
   message: string;
 }
-const useService = () => {
+
+export const useServiceMutation = () => {
   const toast = useToast();
   const [subscribe, { loading }] = useMutation(SUBSCRIBE_MUTATION);
   const [form] = useMutation(FORM_MUTATION);
@@ -85,4 +89,13 @@ const useService = () => {
   return { subscribeEmail, formContact, loading };
 };
 
-export default useService;
+export const useServiceQuery = async () => {
+  const data = await client.query<{ posts: Post[] }>({
+    query: GET_FIRST_FOUR_POSTS_QUERY,
+  });
+  const getFirstFourPosts = (): Post[] => {
+    return data.data.posts;
+  };
+
+  return { getFirstFourPosts };
+};
