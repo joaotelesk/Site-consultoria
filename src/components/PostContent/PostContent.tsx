d8d01d0b0d76bb0a5b3cc0a374c98f8b35d6b36a
Joao Teles <joaojoseteles@gmail.com>
2023-03-13 19:47:52 -0300

    :recycle: refactor: Recebendo e tratados os dados da Api

--- src/components/PostContent/PostContent.tsx
+++ src/components/PostContent/PostContent.tsx
@@ -1,124 +1,115 @@
+import { Post } from "@/interfaces";
+import {
+  convertDatePost,
+  convertImage,
+  convertStringCharacters,
+  convertStringUpcase,
+} from "@/Utilities/Convert";
 import Image from "next/image";
 import Link from "next/link";
+import parse from "html-react-parser";
 
-export default function PostContent() {
+interface PostContentProps {
+  post: Post;
+}
+
+export default function PostContent({ post }: PostContentProps) {
+  const {
+    title,
+    description,
+    postType,
+    createdAt,
+    updatedAt,
+    author,
+    tags,
+    banner,
+    bannerAlt,
+    content,
+  } = post;
+  const updatedDate = convertDatePost(updatedAt);
+  const createdDate = convertDatePost(createdAt);
+
+  console.log(content);
+
   return (
     <>
       <section className=" text-center lg:text-start pt-28 lg:pt-0 lg:mt-40">
-        <div className="container grid grid-cols-1 lg:grid-cols-6 lg:gap-x-11">
-          <div className="col-span-4 ">
+        <div className="container grid grid-cols-1 lg:grid-cols-7 lg:gap-x-11">
+          <div className="col-span-5 ">
             <div className="flex flex-col">
               <span className="font-light flex gap-x-3 text-green-500">
-                <span>Início</span> {">"} <span>Dicas Imigrei</span> {">"}
-                <span className="text-black-400">3 dicas imperdí...</span>
+                <Link href="/blog" className="hover:text-blue-500">
+                  <span>Início</span>
+                </Link>
+                {">"}
+                <Link
+                  href={`/blog/${postType}`}
+                  className="hover:text-blue-500"
+                >
+                  <span>
+                    {postType === "dicas"
+                      ? "Dicas Imigrei"
+                      : convertStringUpcase(postType)}
+                  </span>
+                </Link>
+                {">"}
+                <span className="text-black-400">
+                  {title.length > 14
+                    ? convertStringCharacters(title, 14)
+                    : title}
+                  ...
+                </span>
               </span>
-              <h2 className="mt-9 text-2xl sm:text-4xl text-black-400 font-medium">
-                3 dicas imperdíveis para ter o seu visto em Portugal
+              <h2 className="mt-10 text-2xl sm:text-4xl text-black-400 font-medium">
+                {title}
               </h2>
               <p className="mt-5 font-normal text-gray-300 text-xl">
-                Preparamos este post com incríveis 10 dicar que irá te ajudar, e
-                muito, na retirada de visto para portugal. Aproveite! :)
+                {description}
               </p>
-              <div className="mt-14 flex gap-3">
-                <span className="tags">Dicas Imigrei</span>
-                <span className="tags">Portugal</span>
-                <span className="tags">Imigrantes</span>
+              <div className="mt-7 lg:mt-12 flex gap-3 justify-center lg:justify-start">
+                {tags.length > 0 ? (
+                  tags.map((tag, index) => (
+                    <span key={index} className="tags">
+                      {convertStringUpcase(tag)}
+                    </span>
+                  ))
+                ) : (
+                  <span className="tags">{convertStringUpcase(postType)}</span>
+                )}
               </div>
             </div>
-            <div className="mt-5 flex justify-between">
+            <div className="mt-5 text-sm  lg:text-base  items-start flex lg:flex-row justify-between lg:items-center">
               <p className="text-gray-300">
-                <span className="text-black-400">Criado</span> em 03 AGO de 2022
+                <span className="text-black-400">Criado </span>
+                {createdDate}
               </p>
               <p className="text-gray-300">
-                <span className="text-black-400">Atualizado</span> em 15 DEZ de
-                2022
+                <span className="text-black-400">Atualizado </span>{" "}
+                {updatedDate}
               </p>
               <p className="text-gray-300">
-                <span className="text-black-400">Escrito</span> por Camila de
-                Souza
+                <span className="text-black-400">Escrito</span> por{" "}
+                {convertStringUpcase(author)}
               </p>
             </div>
             <Image
               width={752}
               height={414}
-              src="/Blog/janela.png"
-              alt="Foto da Janela de um avião. Fotografia de Alan Lima"
+              src={`${banner}`}
+              alt={bannerAlt}
               className="w-full mt-16 mb-4"
             />
-            <span className=" text-gray-300">
-              Foto da Janela de um avião. Fotografia de Alan Lima
-            </span>
+            <span className=" text-gray-300">{bannerAlt}</span>
             <article className="text-gray-300 my-10 flex flex-col gap-16 text-xl text-start">
-              <span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-                commodo felis a risus condimentum pellentesque. Pellentesque et
-                diam vel nunc scelerisque molestie. Pellentesque eget risus ut
-                urna bibendum consequat non a erat. Suspendisse rutrum dictum
-                urna, aliquam ullamcorper mi. Integer id nulla porttitor nisi
-                pellentesque tristique ac eu nulla. Sed pellentesque massa vitae
-                diam iaculis, eget luctus urna gravida.
-              </span>
-              <div>
-                <h3 className="text-black-400 text-2xl font-medium mb-4">
-                  1 - Lorem ipsum
-                </h3>
-                <span>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-                  commodo felis a risus condimentum pellentesque. Pellentesque
-                  et diam vel nunc scelerisque molestie. Pellentesque eget risus
-                  ut urna bibendum consequat non a erat. Suspendisse rutrum
-                  dictum urna, aliquam ullamcorper mi. Integer id nulla
-                  porttitor nisi pellentesque tristique ac eu nulla. Sed
-                  pellentesque massa vitae diam iaculis, eget luctus urna
-                  gravida.
-                </span>
-              </div>
-              <div>
-                <h3 className="text-black-400 text-2xl font-medium mb-4">
-                  2 - Lorem ipsum
-                </h3>
-                <span>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-                  commodo felis a risus condimentum pellentesque. Pellentesque
-                  et diam vel nunc scelerisque molestie. Pellentesque eget risus
-                  ut urna bibendum consequat non a erat. Suspendisse rutrum
-                  dictum urna, aliquam ullamcorper mi. Integer id nulla
-                  porttitor nisi pellentesque tristique ac eu nulla. Sed
-                  pellentesque massa vitae diam iaculis, eget luctus urna
-                  gravida.
-                </span>
-                <ul className="mt-4 flex flex-col gap-3">
-                  <li>
-                    Actum urna, aliquam ullamcorper mi. Integer id nulla
-                    porttitor nisi pellentesque tristique!
-                  </li>
-                  <li>
-                    Actum urna, aliquam ullamcorper mi. Integer id nulla
-                    porttitor nisi pellentesque tristique!
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h3 className="text-black-400 text-2xl font-medium mb-4">
-                  3 - Lorem ipsum
-                </h3>
-                <span>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-                  commodo felis a risus condimentum pellentesque.
-                </span>
-                <Image
-                  width={752}
-                  height={414}
-                  src="/Blog/janela.png"
-                  alt="Foto da Janela de um avião. Fotografia de Alan Lima"
-                  className="w-full mt-8 mb-4"
-                />
+              <div className="content-post">
+                {" "}
+                {parse(content, { replace: convertImage })}
               </div>
             </article>
           </div>
 
           <div className="hidden lg:block  lg:col-span-2">
-            <div className="h-auto bg-white-400 flex flex-col px-9 py-10">
+            <div className="h-auto bg-white-400 flex flex-col px-5 py-10">
               <span className="mt-4 text-green-500 font-medium">Website</span>
               <h2 className="mt-2 text-4xl text-black-400 font-medium">
                 Conheça as novidades do website da Imigrei!
