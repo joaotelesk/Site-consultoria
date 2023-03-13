d11f7e4711e30c94e336601db0c576e2fc2ba90f
Joao Teles <joaojoseteles@gmail.com>
2023-03-13 19:37:49 -0300

    :sparkles: feat: arquivo utilities/Corvert

---
+++ src/Utilities/Convert.tsx
@@ -0,0 +1,37 @@
+import { format } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import Image from "next/image";
+
+export function convertDatePost(date: Date | string) {
+  const formatted = format(new Date(date), `'em' dd MMM 'de' yyyy`, {
+    locale: ptBR,
+  });
+
+  const newFormatted = formatted
+    .split(" ")
+    .map((word, index) => (index === 2 ? word.toUpperCase() : word))
+    .join(" ");
+
+  return newFormatted;
+}
+
+export function convertStringUpcase(text: string) {
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+export function convertStringCharacters(text: string, num: number) {
+  return text.substring(0, num);
+}
+export function convertImage({ name, attribs }: any) {
+  if (name === "img") {
+    return (
+      <Image
+        src={attribs.src}
+        width={attribs.width}
+        height={attribs.height}
+        alt={attribs.alt || "Image não possui texto alternativo"}
+      />
+    );
+  }
+  return null;
+}
