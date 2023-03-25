0796f1232de1f817eba9b2c6d61acb4f313edf0e
Joao Teles <joaojoseteles@gmail.com>
2023-03-25 15:27:18 -0300

    :sparkles: feat: variaveis ServiceDataVisto com os dados da pagina de visto e link para pagina de visto-portugues

--- src/Utilities/Variables.ts
+++ src/Utilities/Variables.ts
@@ -28,8 +28,67 @@ export const ServiceData = [
     title: "Visto Portugues",
     description:
       "Oferecemos uma variedade de serviços para os nossos assessorados que buscam o sonho de viver em Portugal e Itália!",
-    link: "#",
+    link: "/servicos/visto-portugues",
     image: "/Home/service3.webp",
     alt: "imagem de uma mulher referente a servicos cidadania Portuguesa",
+  },
+];
+
+export const ServiceDataVisto = [
+  {
+    title: "Procura de Trabalho",
+    description:
+      "Visto que possibilita que brasileiros permaneçam em Portugal por 120 dias, com a possibilidade de estender mais 60 dias, em busca de trabalho.",
+    link: "#",
+    image: "/Servicos/figure6.webp",
+    alt: "texto de imamgem alternativo",
+  },
+  {
+    title: "Procura de Trabalho",
+    description:
+      "Visto que possibilita que brasileiros permaneçam em Portugal por 120 dias, com a possibilidade de estender mais 60 dias, em busca de trabalho.",
+    link: "#",
+    image: "/Servicos/figure6.webp",
+    alt: "texto de imamgem alternativo",
+  },
+  {
+    title: "Procura de Trabalho",
+    description:
+      "Visto que possibilita que brasileiros permaneçam em Portugal por 120 dias, com a possibilidade de estender mais 60 dias, em busca de trabalho.",
+    link: "#",
+    image: "/Servicos/figure6.webp",
+    alt: "texto de imamgem alternativo",
+  },
+  {
+    title: "Procura de Trabalho",
+    description:
+      "Visto que possibilita que brasileiros permaneçam em Portugal por 120 dias, com a possibilidade de estender mais 60 dias, em busca de trabalho.",
+    link: "#",
+    image: "/Servicos/figure6.webp",
+    alt: "texto de imamgem alternativo",
+  },
+  {
+    title: "Procura de Trabalho",
+    description:
+      "Visto que possibilita que brasileiros permaneçam em Portugal por 120 dias, com a possibilidade de estender mais 60 dias, em busca de trabalho.",
+    link: "#",
+    image: "/Servicos/figure6.webp",
+    alt: "texto de imamgem alternativo",
+  },
+  {
+    title: "Procura de Trabalho",
+    description:
+      "Visto que possibilita que brasileiros permaneçam em Portugal por 120 dias, com a possibilidade de estender mais 60 dias, em busca de trabalho.",
+    link: "#",
+    image: "/Servicos/figure6.webp",
+    alt: "texto de imamgem alternativo",
+  },
+  {
+    title: "Procura de Trabalho",
+    description:
+      "Visto que possibilita que brasileiros permaneçam em Portugal por 120 dias, com a possibilidade de estender mais 60 dias, em busca de trabalho.",
+    link: "#",
+    image: "/Servicos/figure6.webp",
+    alt: "texto de imamgem alternativo",
   },
 ];
